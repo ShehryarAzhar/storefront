f046604700a4f25a4b732c30a1f8d228369ffb38
ShehryarAzhar <m.shehryarazhar@outlook.com>
2026-01-13 18:43:43 +0500

    Seperate Configuration settings for Dev and Prod and Provision Heroku Add-ons

--- storefront/settings/prod.py
+++ storefront/settings/prod.py
@@ -1,9 +1,34 @@
 from .common import *
 import os
+import dj_database_url
 
 
 DEBUG = False
 
 SECRET_KEY = os.environ['SECRET_KEY']
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['sherybuy-prod-8c19f1a2a82c.herokuapp.com']
+
+DATABASES = {
+    'default': dj_database_url.config()
+}
+
+REDISCLOUD_URL = os.environ['REDISCLOUD_URL']
+
+CELERY_BROKER_URL = REDISCLOUD_URL
+
+CACHES = {
+    "default": {
+        "BACKEND": "django_redis.cache.RedisCache",
+        "LOCATION": REDISCLOUD_URL,
+        "TIMEOUT": 10 * 60,
+        "OPTIONS": {
+            "CLIENT_CLASS": "django_redis.client.DefaultClient",
+        }
+    }
+}
+
+EMAIL_HOST = os.environ['MAILGUN_SMTP_SERVER']
+EMAIL_HOST_USER = os.environ['MAILGUN_SMTP_LOGIN']
+EMAIL_HOST_PASSWORD = os.environ['MAILGUN_SMTP_PASSWORD']
+EMAIL_PORT = os.environ['MAILGUN_SMTP_PORT']
